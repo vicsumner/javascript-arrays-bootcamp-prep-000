efcacbaf788b2c1e7728ece97b4ee8d9ba9ae2c8
Victoria L Sumner <vicsumner@gmail.com>
2018-10-15 22:07:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,7 +19,8 @@ function destructivelyAddElementToEndOfArray(array, element){
 }
 
 function accessElementInArray(array, index){
-  return accessElementInArray [array], index
+  accessElementInArray [array], index
+  return array
 }
 function destructivelyRemoveElementFromBeginningOfArray (array){
   array.shift(1)
